1e3e22ec712c71407621ee344732ff1a063e8795
vinhnt <kieuphong2840@gmail.com>
2024-03-25 23:37:43 +0700

    add history table view

--- reacjs-sample/src/pages/Home/HistoryListView.js
+++ reacjs-sample/src/pages/Home/HistoryListView.js
@@ -1,5 +1,20 @@
 import { useEffect, useState } from "react";
-import { Box, Card, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  Checkbox,
+  Container,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Typography,
+} from "@mui/material";
 import ExpenseCard from "./ExpenseCard";
 import { callGetExpenseList } from "../ExpenseInput/ExpenseApi";
 import dayjs from "dayjs";
@@ -23,17 +38,41 @@ export default function HistoryListView() {
   }, []);
 
   return (
-    <Box>
+    <Container
+      sx={{
+        m: 2,
+      }}
+    >
       <Stack spacing={2}>
-        <Typography>History</Typography>
-
-        {expenseList.map((e, index) => (
-          // <Card>
-          //   <Typography>{JSON.stringify(e)}</Typography>
-          // </Card>
-          <ExpenseCard key={index} expense={e} />
-        ))}
+        <Typography variant="h4">Lịch sử</Typography>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Ngày</TableCell>
+                <TableCell align="right">Chi tiêu</TableCell>
+                <TableCell align="right">Loại chi tiêu</TableCell>
+                <TableCell align="right">Nguồn tiền</TableCell>
+                <TableCell align="right">Ghi chú</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {expenseList.map((row, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="left">{row.date}</TableCell>
+                  <TableCell align="right">{row.amount}</TableCell>
+                  <TableCell align="right">{row.category.name}</TableCell>
+                  <TableCell align="right">{row.source.name}</TableCell>
+                  <TableCell align="right">{row.description}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Stack>
-    </Box>
+    </Container>
   );
 }
